d2095e140312cc8e60537216897437b4b82ece58
ranieriiuri <iuriranierioliveira@gmail.com>
2025-01-28 17:12:35 -0300

    complementing 'Pacient' and 'Prescription' models

---
+++ models/Prescription.js
@@ -0,0 +1,33 @@
+import {  mongoose  } from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const prescriptionSchema = new Schema({
+    date: {
+        type: Date,
+        required: [true, 'Date of Prescription is required.']
+    },
+    appointementId: {
+        type: String,
+        required: [true, 'Appointment ID is required.']
+    },
+    medicine: {
+        type: String,
+        required: [true, 'Medicine is required.']
+    },
+    dosage: {
+        type: String,
+        required: [true, 'Dosage is required.']
+    },
+    instructions: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const prescription = mongoose.model('Prescription', prescriptionSchema);
+
+export default prescription;
