df912a8a1ef6a081ccc4c847948bbab6ce843526
ranieriiuri <iuriranierioliveira@gmail.com>
2025-01-30 15:18:31 -0300

    fixing DoctorService and adding module dependencies on package.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import pkg from "body-parser";
 import router from "./routes/router.js";
 import db from "./database/database.js";
+import cors from "cors";
 
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = 3000;
 
 app.use(json());
 app.use(urlencoded({extended: true}));
+app.use(cors());
 
 app.listen(PORT, function() {
     console.log(`Listening to port ${PORT}`);
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
 import express from "express";
 import appointmentController from "./AppointmentController.js";
-// import doctorController from "./DoctorController";
-// import pacientController from "./PacientController";
-// import prescriptionController from "./PrescriptionController";
+import doctorController from "./DoctorController.js";
+import pacientController from "./PacientController.js";
+import prescriptionController from "./PrescriptionController.js";
 
 
 //Essa var utitliza um recurso do express p fazer o roteamento automático
@@ -16,12 +16,10 @@ router.get(
     }
 );
 
-//estamos basicamente falando pro router do express p usar o / para controlar todas as funções da aplicação
+//estamos basicamente falando pro router do express p usar o / para controlar todas as funções da aplicação. E cada controller especifica sua rota...
 router.use("/", appointmentController);
-// *Descomentar rotas abaixo conforme andamento do projeto!
-// router.use("/", doctorController);
-// router.use("/", pacientController);
-// router.use("/", prescriptionController);
-
+router.use("/", doctorController);
+router.use("/", pacientController);
+router.use("/", prescriptionController);
 
 export default router;
